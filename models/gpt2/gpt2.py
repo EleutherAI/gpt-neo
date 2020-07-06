bea1a459d740bb716bedbe894ffe6539c59ac95c
sdtblck <sdtblck@gmail.com>
2020-07-06 18:48:28 +0200

    Got rid of shitty transformer code, added minimal mnist example for reference & testing. Started porting GPT.

--- models/gpt2/gpt2.py
+++ models/gpt2/gpt2.py
@@ -2,24 +2,29 @@ import math
 
 import numpy as np
 import tensorflow as tf
-
+import mesh_tensorflow as mtf
+import os
 
 def shape_list(x):
+    # TODO: can this be used with mtf? tensor shapes are different
     """Deal with dynamic shape in tensorflow cleanly."""
     static = x.shape.as_list()
     dynamic = tf.shape(x)
     return [dynamic[i] if s is None else s for i, s in enumerate(static)]
 
 def softmax(x, axis=-1):
-    x = x - tf.reduce_max(x, axis=axis, keepdims=True)
-    ex = tf.exp(x)
-    return ex / tf.reduce_sum(ex, axis=axis, keepdims=True)
+    # x = x - tf.reduce_max(x, axis=axis, keepdims=True)
+    # ex = tf.exp(x)
+    # return ex / tf.reduce_sum(ex, axis=axis, keepdims=True)
+    return mtf.softmax(x, axis)
 
 def gelu(x):
-    return 0.5*x*(1+tf.tanh(np.sqrt(2/np.pi)*(x+0.044715*tf.pow(x, 3))))
+    # return 0.5*x*(1+tf.tanh(np.sqrt(2/np.pi)*(x+0.044715*tf.pow(x, 3))))
+    return mtf.gelu(x)
 
 def norm(x, scope, *, axis=-1, epsilon=1e-5, params=None):
     """Normalize to mean = 0, std = 1, then do a diagonal affine transform."""
+    # TODO: convert to mtf code
     with tf.variable_scope(scope):
         n_state = x.shape[-1].value
         if params["precision"] == "bfloat16":
@@ -36,15 +41,20 @@ def norm(x, scope, *, axis=-1, epsilon=1e-5, params=None):
 
 def split_states(x, n):
     """Reshape the last dimension of x into [n, x.shape[-1]/n]."""
+    # TODO: convert to mtf code
     *start, m = shape_list(x)
     return tf.reshape(x, start + [n, m//n])
 
 def merge_states(x):
     """Smash the last two dimensions of x into a single dimension."""
+    # TODO: convert to mtf code
     *start, a, b = shape_list(x)
     return tf.reshape(x, start + [a*b])
 
 def conv1d(x, scope, nf, *, w_init_stdev=0.02, params=None, scale=False):
+    # TODO: convert to mtf code
+    #  (mtf.layers.conv1d is a thing but i think we want to keep scale_by_depth / scale_by_in opts)
+
     if params["scale_by_depth"] and scale: # Scale by sqrt(num_layers), only happens at the final projection before a res block output
         w_init_stdev = w_init_stdev * (1. / math.sqrt(params["n_layer"]))
     if params["scale_by_in"]: # Scale by sqrt(num_input_features)
@@ -62,6 +72,7 @@ def conv1d(x, scope, nf, *, w_init_stdev=0.02, params=None, scale=False):
         return c
 
 def attention_mask(nd, ns, *, dtype):
+    # TODO: convert to mtf code
     """1's in the lower triangle, counting from the lower right corner.
 
     Same as tf.matrix_band_part(tf.ones([nd, ns]), -1, ns-nd), but doesn't produce garbage on TPUs.
@@ -73,6 +84,7 @@ def attention_mask(nd, ns, *, dtype):
 
 
 def attn(x, scope, n_state, *, past, params, block_offset=0, train=False):
+    # TODO: convert to mtf code. There are some impls of local attention but not sure if they do exactly the same thing
     assert x.shape.ndims == 3  # Should be [batch, sequence, features]
     assert n_state % params["n_head"] == 0
     if past is not None:
@@ -108,14 +120,18 @@ def attn(x, scope, n_state, *, past, params, block_offset=0, train=False):
         x = tf.reshape(x, [sh_batch * params["fixed_attn_block_size"], padded_seq // params["fixed_attn_block_size"], params["n_embd"]]) # should be [batch * blocks, sequence / blocks, features]
 
     def split_heads(x):
+        # TODO: convert to mtf code
+
         # From [batch, sequence, features] to [batch, heads, sequence, features]
         return tf.transpose(split_states(x, params["n_head"]), [0, 2, 1, 3])
 
     def merge_heads(x):
+        # TODO: convert to mtf code
         # Reverse of split_heads
         return merge_states(tf.transpose(x, [0, 2, 1, 3]))
 
     def mask_attn_weights(w):
+        # TODO: convert to mtf code
         # w has shape [batch, heads, dst_sequence, src_sequence], where information flows from src to dst.
         _, _, nd, ns = shape_list(w)
         b = attention_mask(nd, ns, dtype=w.dtype)
@@ -124,6 +140,9 @@ def attn(x, scope, n_state, *, past, params, block_offset=0, train=False):
         return w
 
     def multihead_attn(q, k, v):
+        #TODO: convt to mtf code - already implemented at:
+        # mtf.layers.multihead_attention(query_antecedent, memory_antecedent, mask, kv_channels, heads, *))
+
         # q, k, v have shape [batch, heads, sequence, features]
         w = tf.matmul(q, k, transpose_b=True)
         w = w * tf.rsqrt(tf.cast(v.shape[-1].value, w.dtype))
@@ -169,6 +188,7 @@ def attn(x, scope, n_state, *, past, params, block_offset=0, train=False):
 
 def mlp(x, scope, n_state, *, params, train=False):
     with tf.variable_scope(scope):
+        # TODO: nx will probably be the only thing that needs changing here
         nx = x.shape[-1].value
         h = gelu(conv1d(x, 'c_fc', n_state, params=params))
         h2 = conv1d(h, 'c_proj', nx, params=params, scale=True)
@@ -186,63 +206,89 @@ def block(x, scope, *, past, params, train=False, block_offset=0):
         return x, present
 
 def past_shape(*, params, batch_size=None, sequence=None):
-    return [batch_size, params["n_layer"], 2, params["n_head"], sequence, params["n_embd"] // params["n_head"]]
+    # TODO: think this should be converted to mtf.Shape( return ), but not sure
+    # return [batch_size, params["n_layer"], 2, params["n_head"], sequence, params["n_embd"] // params["n_head"]]
+    return mtf.Shape([batch_size, params["n_layer"], 2, params["n_head"], sequence, params["n_embd"] // params["n_head"]])
 
 def expand_tile(value, size):
     """Add a new axis of given size."""
+    # TODO: convert to mtf code
     value = tf.convert_to_tensor(value, name='value')
     ndims = value.shape.ndims
     return tf.tile(tf.expand_dims(value, axis=0), [size] + [1]*ndims)
 
 def positions_for(tokens, past_length):
+    # TODO: convert to mtf.shape ?
     batch_size = tf.shape(tokens)[0]
     nsteps = tf.shape(tokens)[1]
     return expand_tile(past_length + tf.range(nsteps), batch_size)
 
 def dropout(x, pdrop, train):
     if train and pdrop > 0:
-        x = tf.nn.dropout(x, rate=pdrop)
+        # x = tf.nn.dropout(x, rate=pdrop)
+        x = mtf.dropout(x, rate=pdrop)
     return x
 
 def _assert_float_dtype(dtype):
+    # no usages
     if not dtype.is_floating:
         raise ValueError("Expected floating point type, got %s." % dtype)
     return dtype
 
 
-def model(X, params, labels=None, past=None, scope='model', reuse=False, train=False):
+def model(X, params, mesh=None, labels=None, past=None, scope='model', reuse=False, train=False):
     with tf.variable_scope(scope, reuse=reuse):
+        if os.environ.get('DEBUG', 0):
+            print('INPUT SHAPE:')
+            print(X.shape)
         results = {}
         batch, sequence = shape_list(X)
 
+        # define mtf shapes and names
+        batch_size = os.environ.get('BATCH_SIZE', 0)
+        sequence_size = os.environ.get('SEQUENCE', 0)
+        assert batch_size > 0
+        batch_dim = mtf.Dimension("batch", batch_size)
+        sequence_dim = mtf.Dimension("sequence", sequence_size)
+
+        X = mtf.import_tf_tensor(mesh, X, mtf.Shape(X.shape)) # convert input tensor to mtf tensor
+
         if params["precision"] == "bfloat16":
-            wpe = tf.get_variable('wpe', [params["n_ctx"], params["n_embd"]], # Position encoding
+            wpe = mtf.get_variable('wpe', mtf.Shape([params["n_ctx"], params["n_embd"]]), # Position encoding
                              initializer=tf.random_normal_initializer(stddev=0.01, dtype=tf.bfloat16), dtype=tf.bfloat16)
-            wte = tf.get_variable('wte', [params["n_vocab"], params["n_embd"]], # Text encoding
+            wte = mtf.get_variable('wte', mtf.Shape([params["n_vocab"], params["n_embd"]]), # Text encoding
                              initializer=tf.random_normal_initializer(stddev=0.02, dtype=tf.bfloat16), dtype=tf.bfloat16)
 
         else:
-            wpe = tf.get_variable('wpe', [params["n_ctx"], params["n_embd"]], # Position encoding
+            wpe = mtf.get_variable('wpe', mtf.Shape([params["n_ctx"], params["n_embd"]]), # Position encoding
                                 initializer=tf.random_normal_initializer(stddev=0.01))
-            wte = tf.get_variable('wte', [params["n_vocab"], params["n_embd"]], # Text encoding
+            wte = mtf.get_variable('wte', mtf.Shape([params["n_vocab"], params["n_embd"]]), # Text encoding
                                 initializer=tf.random_normal_initializer(stddev=0.02))
-        past_length = 0 if past is None else tf.shape(past)[-2]
+
+        past_length = 0 if past is None else tf.shape(past)[-2] # TODO: convert to mtf shape
 
         wpe = dropout(wpe, params["embed_dropout"], train)
         wte = dropout(wte, params["embed_dropout"], train)
 
-        h = tf.gather(wte, X) + tf.gather(wpe, positions_for(X, past_length))
+        # TODO: convert positions_for to mtf code
+        h = mtf.gather(wte, X, 0) + mtf.gather(wpe, positions_for(X, past_length), 0)
 
         # Transformer
         presents = []
-        pasts = tf.unstack(past, axis=1) if past is not None else [None] * params["n_layer"]
+        pasts = mtf.unstack(past, dim=1) if past is not None else [None] * params["n_layer"]
         assert len(pasts) == params["n_layer"]
-        for layer, past in enumerate(pasts):
+        for layer, past in enumerate(pasts): # TODO: will enumerate work with mtf tensors?
             h, present = block(h, 'h%d' % layer, past=past, params=params, block_offset=(layer * params["layer_offset"]) % params["fixed_attn_block_size"])
             presents.append(present)
-        results['present'] = tf.stack(presents, axis=1)
+        dim_name = "xxxx" # TODO: need to define dim_name for stack - why ? ? (╯°□°)╯︵ ┻━┻
+        results['present'] = mtf.stack(presents, dim_name=dim_name, axis=1)
         h = norm(h, 'ln_f', params=params)
 
+        # TODO: optimization suggestion from bmk:
+        # optimize by putting lots of sparse layers next to each other to reduce reshapes,
+        # and only reshape between sparse and regular layers instead of resizing every time for drop in compatibility
+
+        # TODO: convert below to mtf fns
         h_flat = tf.reshape(h, [batch*sequence, params["n_embd"]])
         logits = tf.matmul(h_flat, wte, transpose_b=True)
         logits = tf.reshape(logits, [batch, sequence, params["n_vocab"]])
