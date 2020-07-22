cc35bcc2594947b9b25bd4e1ec6f7af101ddd295
sdtblck <sdtblck@gmail.com>
2020-07-22 15:38:59 +0200

    update bash scripts

--- run_v8.sh
+++ run_v8.sh
@@ -1,7 +1,7 @@
 bash make_hosts_wrapper
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/lib
 while true; do
-  python3 main.py --tpu sanchopanza --model_params configs/v8_test.json --steps_per_checkpoint 100
+  python3 main.py --tpu sanchopanza --model configs/v8_test.json --steps_per_checkpoint 100
   echo "Recreating sanchopanza in 60sec..."
   sleep 60
   pu recreate sanchopanza --yes
