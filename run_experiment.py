40a4107face0bd5ba28ee99928229dfb2c3a76d4
Leo Gao <leogao31@gmail.com>
2020-08-12 17:05:51 -0600

    add tensorboard

--- run_experiment.py
+++ run_experiment.py
@@ -8,7 +8,14 @@ import os
 import json 
 
 ex = sacred.Experiment('eleutherai-gpt3')
-#ex.observers.append(sacred.observers.QueuedMongoObserver(url='127.0.0.1:27017', db_name='db', username='user', password='password'))
+ex.observers.append(sacred.observers.QueuedMongoObserver(url='127.0.0.1:27017', db_name='db', username='user', password='password'))
+
+import socket
+def get_open_port(lo=8000, hi=8100):
+    for i in range(lo, hi):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            if s.connect_ex(('localhost', i)) != 0:
+                return i
 
 
 @ex.main
@@ -20,19 +27,25 @@ def main(_run):
     parser.add_argument('--model', type=str) # JSON file that contains model parameters
     args = parser.parse_args()
 
-    ex.add_config({
-        'tpu_name': args.tpu,
-        **json.load(open(args.model))
-    })
-
     os.makedirs('run_configs', exist_ok=True)
     shutil.copy(args.model, 'run_configs/config_{}.json'.format(_run._id))
+
+    params = json.load(open('run_configs/config_{}.json'.format(_run._id)))
+    ex.add_config({
+        'tpu_name': args.tpu,
+        **params
+    })
+
+    tensorboard_port = get_open_port()
+    print('Tensorboard at port:', tensorboard_port)
+    os.system("tensorboard --logdir {} --port {}".format(params["model_path"], tensorboard_port))
 
     while True:
         os.system("python3 main.py --tpu {tpu} --model run_configs/config_{id}.json".format(tpu=args.tpu, id=_run._id))
         print("Recreating {} in 60sec...".format(args.tpu))
         time.sleep(60)
 
+        os.system("python3 main.py --tpu {tpu} --model run_configs/config_{id}.json".format(tpu=args.tpu, id=_run._id))
 
 
 if __name__ == '__main__':
