1924413a3c90f88094caca65167b1d117762de5c
Leo Gao <leogao31@gmail.com>
2020-08-12 16:44:32 -0600

    Add sacred script

---
+++ run_experiment.py
@@ -0,0 +1,31 @@
+import sacred
+import argparse
+import main
+import time
+import subprocess
+import shutil
+import os
+
+ex = sacred.Experiment('eleutherai-gpt3')
+#ex.observers.append(sacred.observers.QueuedMongoObserver(url='127.0.0.1:27017', db_name='db', username='user', password='password'))
+
+
+@ex.main
+def main(_run):
+    print('Starting run', _run._id)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tpu', type=str) # Name of TPU to train on, if any
+    parser.add_argument('--model', type=str) # JSON file that contains model parameters
+    args = parser.parse_args()
+
+    os.makedirs('run_configs', exist_ok=True)
+    shutil.copy(args.model, 'run_configs/config_{}.json'.format(_run._id))
+
+    while True:
+        os.system("python3 main.py --tpu {tpu} --model run_configs/config_{id}.json".format(tpu=args.tpu, id=_run._id))
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    ex.run()
