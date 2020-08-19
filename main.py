73bebdc56ef0d512b3823ab45f3a17969afe55c0
Phil Wang <lucidrains@gmail.com>
2020-08-19 08:46:37 -0700

    remove logging hook and session config

--- main.py
+++ main.py
@@ -89,22 +89,20 @@ def main():
     # Set up TPUs and Estimator
 
     tpu_cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(args.tpu) if params["use_tpu"] else None
+
     config = tpu_config.RunConfig(
         cluster=tpu_cluster_resolver,
         model_dir=params["model_path"],
         save_checkpoints_steps=None,  # Disable the default saver
         save_checkpoints_secs=None,  # Disable the default saver
         log_step_count_steps=params["iterations"],
-        session_config=tf.ConfigProto(
-            allow_soft_placement=True,
-            log_device_placement=True
-        ),
         save_summary_steps=params["iterations"],
         tpu_config=tpu_config.TPUConfig(
             num_shards=mesh_shape.size,
             iterations_per_loop=params["iterations"],
             num_cores_per_replica=1,
             per_host_input_for_training=tpu_config.InputPipelineConfig.BROADCAST))
+
     estimator = tpu_estimator.TPUEstimator(
         use_tpu=params["use_tpu"],
         model_fn=model_fn,
