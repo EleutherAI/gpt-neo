3c7783f57bf07ce33f9a9d291f1fe4e6c74a24a9
sdtblck <sdtblck@gmail.com>
2020-08-20 02:38:36 +0200

    fix error if stop_at_token is None

--- sample.py
+++ sample.py
@@ -113,9 +113,10 @@ def sample_autoregressive(partial_sequences,
         partial_sequences_eos_count = 0
     else:
         initial_states = context_first_part.new_states
-    partial_sequences_eos_count = mtf.reduce_sum(
-        mtf.to_int32(mtf.equal(partial_sequences, stop_at_token)),
-        reduced_dim=length_dim)
+    if stop_at_token is not None:
+        partial_sequences_eos_count = mtf.reduce_sum(
+            mtf.to_int32(mtf.equal(partial_sequences, stop_at_token)),
+            reduced_dim=length_dim)
 
     def cond_fn(position, ids, *unused_states):
         """Should we run another loop iteration."""
