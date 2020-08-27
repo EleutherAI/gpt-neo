0ebc2eb9d2d7e3628bff8b3aa027c63eed4e0c62
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2020-08-27 16:50:52 -0700

    add lang filter

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM continuumio/miniconda3
 
 WORKDIR /neogpt
 ADD requirements.txt .
+RUN conda install gcc_linux-64 gxx_linux-64 -y
 RUN pip install -r requirements.txt
 
 CMD /bin/bash
---
+++ scripts/filter_lang.py
@@ -0,0 +1,36 @@
+import fasttext
+import os
+import sys
+from absl import app, logging, flags
+
+FLAGS = flags.FLAGS
+# https://fasttext.cc/docs/en/language-identification.html
+VALID_LANGS ='af als am an ar arz as ast av az azb ba bar bcl be bg bh bn bo bpy br bs bxr ca cbk ce ceb ckb co cs cv cy da de diq dsb dty dv el eml en eo es et eu fa fi fr frr fy ga gd gl gn gom gu gv he hi hif hr hsb ht hu hy ia id ie ilo io is it ja jbo jv ka kk km kn ko krc ku kv kw ky la lb lez li lmo lo lrc lt lv mai mg mhr min mk ml mn mr mrj ms mt mwl my myv mzn nah nap nds ne new nl nn no oc or os pa pam pfl pl pms pnb ps pt qu rm ro ru rue sa sah sc scn sco sd sh si sk sl so sq sr su sv sw ta te tg th tk tl tr tt tyv ug uk ur uz vec vep vi vls vo wa war wuu xal xmf yi yo yue zh'.split()
+
+flags.DEFINE_multi_string('lang', default='en', help='language to include. can be specified multiple time for multi language')
+flags.DEFINE_float('threshold', default=0.99, help='language to include. can be specified multiple time for multi language')
+flags.DEFINE_boolean('debug', False, 'Produces debugging output.')
+
+ext = '.bin'
+if not os.path.exists('lid.176' + ext): 
+    logging.info('downloading model')
+    os.system('wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176' + ext)
+    logging.info('downloading model: done')
+
+lid_model = fasttext.load_model('lid.176' + ext)
+
+def main(_):
+    while True:
+        line = sys.stdin.readline()
+        if not line: break
+        
+        (labels, scores)= lid_model.predict(line.strip())
+        lang = labels[0].split("__label__")[1]
+        score = scores[0]
+        if lang in FLAGS.lang and score > FLAGS.threshold:
+            sys.stdout.write(line)
+        elif FLAGS.debug:
+            logging.info(str(score, lang, FLAGS.lang, FLAGS.threshold))
+
+if __name__ == '__main__':
+    app.run(main)
