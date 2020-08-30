8f6ed828e542e863ce8f8cfbe21f0a828e2568ef
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2020-08-30 03:50:22 +0000

    fix report

--- scripts/check_dataset.py
+++ scripts/check_dataset.py
@@ -1,3 +1,4 @@
+import os
 import random
 import collections
 
@@ -5,6 +6,7 @@ import tensorflow as tf
 from absl import app, logging
 from absl.flags import argparse_flags
 from tokenizers import Tokenizer
+from transformers import GPT2Config, GPT2Tokenizer, GPT2TokenizerFast
 
 PreProcessedTextLine = collections.namedtuple('PreProcessedTextLine', ['id', 'content', 'target', 'offset_start', 'offset_end'])
 
@@ -12,24 +14,48 @@ def parse_args(argv):
     parser = argparse_flags.ArgumentParser()
     parser.add_argument("--input", type=str, required=True, help="Path to where your files are located. Files ending in .zst are treated as \
                         archives, all others as raw text.")
-    parser.add_argument("--tokenizer", type=str, default="byte-level-bpe.tokenizer.json", help="Name or path of a tokenizer spec")
+    parser.add_argument("--tokenizer", type=str, required=True, help="Name or path of a tokenizer spec")
     parser.add_argument("--random_seed", type=int, default=1337, help="seed")
     parser.add_argument("--sample_size", type=int, default=10, help="the size of samples to inspect")
     args = parser.parse_args(argv[1:])
     return args
 
 def load_tokenizer(location):
-    return Tokenizer.from_file(location)
+    if tf.io.gfile.exists(location):
+        # use tf gfile in case the dictionary is remote
+        buff = tf.io.gfile.GFile(location).read()
+        tok = Tokenizer.from_buffer(buff)
+        model_location = os.path.split(location)[0]
+        tok.model.save(model_location)
+        fastok = GPT2TokenizerFast.from_pretrained(model_location)
+        fastok.add_special_tokens({
+            'eos_token': '[EOS]',
+            'pad_token': '[PAD]',
+            'pad_token': '[UNK]',
+        })
+    else:
+        if location.startswith('/'):
+            raise ValueError('invalid location %s', location)
+        else:
+            fastok = GPT2TokenizerFast.from_pretrained(location)
+    return fastok
 
 def read_example(example_proto, max_seq_len=1024) -> dict:
     features = {
-        "id": tf.VarLenFeature(tf.uint64, default=-1),
-        "content": tf.VarLenFeature(tf.bytes, default=0),
-        "target": tf.VarLenFeature(tf.uint64, default=0),
-        "offset_start": tf.VarLenFeature(tf.uint64, default=0),
-        "offset_end": tf.VarLenFeature(tf.uint64, default=0),
+        "id": tf.io.VarLenFeature(tf.int64),
+        "content": tf.io.FixedLenFeature([], tf.string),
+        "target": tf.io.VarLenFeature(tf.int64),
+        "offset_start": tf.io.VarLenFeature(tf.int64),
+        "offset_end": tf.io.VarLenFeature(tf.int64),
     }
-    return tf.parse_single_example(example_proto, features)
+    parsed_features = tf.io.parse_single_example(example_proto, features)
+    return {
+        "id": tf.cast(parsed_features['id'], tf.uint64),
+        "content": parsed_features['content'],
+        "target": tf.sparse.to_dense(tf.cast(parsed_features['target'], tf.int64)),
+        "offset_start": tf.sparse.to_dense(tf.cast(parsed_features['offset_start'], tf.uint64)),
+        "offset_end": tf.sparse.to_dense(tf.cast(parsed_features['offset_end'], tf.uint64)),
+    } 
 
 def main(args):
     tokenizer = load_tokenizer(args.tokenizer)
@@ -49,25 +75,24 @@ def main(args):
             it = ds.make_one_shot_iterator()
             example = it.get_next()
             
-            tensor = example['content'] 
-            max_id_tf = tf.reduce_max(tensor)
-            min_id_tf = tf.reduce_min(tensor)
-            
             while True: 
                 try:
-                    example, max_id, min_id = sess.run([example, max_id_tf, min_id_tf])
-                    
-                    example = PreProcessedTextLine(**example)
+                    result = sess.run(example) #, max_id_tf, min_id_tf])
+                    pt = PreProcessedTextLine(
+                        id = result['id'],
+                        content=result['content'],
+                        target=result['target'],
+                        offset_start=result['offset_start'],
+                        offset_end=result['offset_end'],
+                    )
 
-                    txt = tokenizer.decode(example['content'])
 
-                    print(example['content'].shape)
-                    print('-' * 50)
-                    print(txt[:500], '\n...\n', txt[-500:])
-                    print('-' * 50)
-                    print('min token id: ', min_id)
-                    print('max token id: ', max_id)
-                    print('tokenization:', [pt.content[slice(start,end)] for start,end in zip(pt.offset_start, pt.offset_end)])
+                    ids = tokenizer.decode(result['target'])
+
+                    logging.info('gold text:    %r', pt.content.decode('utf-8'))
+                    logging.info('decoded:       %r', ids),
+                    logging.info('tokenization: %s', [pt.content.decode('utf-8')[slice(int(start), int(end))] for start,end in zip(pt.offset_start, pt.offset_end)])
+                    logging.info('-' * 10)
                 except tf.errors.OutOfRangeError:
                     break
 
