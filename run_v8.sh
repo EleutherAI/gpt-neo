06b194de30ed4a74b0bda2875718842dcf00147d
sdtblck <sdtblck@gmail.com>
2020-09-02 03:03:36 +0200

    cleanup

--- run_v8.sh
+++
@@ -1,8 +0,0 @@
-bash make_hosts_wrapper
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/lib
-while true; do
-  python3 main.py --tpu sanchopanza --model configs/v8_test.json --steps_per_checkpoint 100
-  echo "Recreating sanchopanza in 60sec..."
-  sleep 60
-  pu recreate sanchopanza --yes
-done
