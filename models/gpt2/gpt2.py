488f41a572da9234b3db64ee7bb55a026d08f865
Phil Wang <lucidrains@gmail.com>
2020-09-12 12:19:14 -0700

    do not count padding tokens in loss

--- models/gpt2/gpt2.py
+++ models/gpt2/gpt2.py
@@ -450,16 +450,22 @@ def model(mtf_features, other_features, params, mesh, variable_dtype, context=No
             # equivalent to tf.matmul
             logits = mtf.einsum([h, wte], output_shape=[batch_dim, seq_dim, vocab_dim])
 
-    if params["mode"] is not "predict":
+    if params["mode"] == "train":
         vdim = logits.shape[-1]  # get vocab dimension
         labels = mtf_features["labels"]
         z_loss = params.get('z_loss', 1e-4)
         # go to full precision for the logits 
         logits = mtf.cast(logits, tf.float32)
+
         with tf.variable_scope('xentropy_final'):
             loss_batch = mtf.layers.softmax_cross_entropy_with_logits(logits=logits, targets=labels, vocab_dim=vdim, z_loss=z_loss)
+
+        # make sure labels with padding tokens are not counted in the loss
+        loss_batch = mtf.where(mtf.not_equal(labels, 0), loss_batch, mtf.zeros_like(loss_batch))
+
         with tf.variable_scope('reduce_mean_final'):
             loss = mtf.reduce_mean(loss_batch)
+
         loss += aux_losses  # add on auxiliary losses (currently only used for moe)
         loss /= params["num_microbatches"]
     else:
