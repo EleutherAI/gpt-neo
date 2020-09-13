87ad612b630716775688dfc489d684f45347f52f
Phil Wang <lucidrains@gmail.com>
2020-09-13 12:42:24 -0700

    fix big bug with exclamation mark being used as padding for gpt2

--- encoders.py
+++ encoders.py
@@ -14,7 +14,11 @@ def fetch_encoder(params):
     is_pretrained = dataset.get('tokenizer_is_pretrained', False)
 
     if is_pretrained:
-        return GPT2Tokenizer.from_pretrained(path)
+        tok = GPT2Tokenizer.from_pretrained(path)
+
+        # will add a padding token id of 50257 at run-time
+        tok.add_special_tokens({'pad_token': ' '})
+        return tok
 
     return Tokenizer.from_file(path)
 
