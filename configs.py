8f8499136104b5ef6630975673aedd254921b7cb
sdtblck <sdtblck@gmail.com>
2020-09-16 15:20:29 +0200

    move dataset_configs to configs folder

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ from operator import itemgetter
 
 DATASETS = {}
 
-for path in Path('./dataset_configs').glob('*.json'):
+for path in Path('./configs/dataset_configs').glob('*.json'):
     dataset_id = path.stem
     DATASETS[dataset_id] = json.loads(path.read_text())
 
