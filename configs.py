f9912c0af34a92580a3a1782b8c88618c3ef743a
sid <sidney.black@aleph-alpha.de>
2020-11-01 21:36:08 +0100

    move create_tfrecords.py and train_tokenizer.py into ./data

--- configs.py
+++ configs.py
@@ -1,7 +1,6 @@
 import json
 from pathlib import Path
 from collections import defaultdict
-from operator import itemgetter
 
 DATASETS = {}
 
--- data/train_tokenizer.py
+++ data/train_tokenizer.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument("--base_dir", type=str, help="Path to where your files are located. Files ending in .zst are treated as \
                     archives, all others as raw text.")
-parser.add_argument("--output_dir", type=str, default="tokenizers" help="Where to put the tokenizer")
+parser.add_argument("--output_dir", type=str, default="tokenizers", help="Where to put the tokenizer")
 parser.add_argument("--file_type", type=str, choices=["xz", "txt"], default="xz", help="Extension of file to parse")
 parser.add_argument("--vocab_size", type=int, help="Size of vocabulary", required = True)
 args = parser.parse_args()
