b370be9da727a8a72e892256aa9d5c8377d2eaa9
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-11-06 14:23:28 +0100

    fix(model): use dim_name in stack

--- models/gpt2/gpt2.py
+++ models/gpt2/gpt2.py
@@ -314,7 +314,7 @@ def attn(x, scope, n_state, *, attention_type, params, bias, dim_seq, memory_len
                 a = mtfparams.compute_output(a, x_shape)
             
         if shuffle:
-            a = mtf.stack([a, x0], mtf.Dimension('tmp_stack', 2), 4)
+            a = mtf.stack([a, x0], 'tmp_stack', 4)
             a = mtf.reshape(a, a.shape.dims[:-2] + [original_dim_embd])
 
         if params["mode"] == "train" and params["res_dropout"] > 0:
