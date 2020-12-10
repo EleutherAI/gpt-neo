bfe1dc98b8d2af8ff3ce873fa62df0ecad4a0866
XMaster96 <XMaster96@gmx.de>
2020-12-10 03:17:03 +0100

    test from inputs.py v2

--- inputs.py
+++ inputs.py
@@ -3,16 +3,18 @@ import tensorflow.compat.v1 as tf
 from functools import partial
 from data.encoders import encode
 import os
+import random
 import requests
 from data.video2tfrecord import frame_decoder
 from google.cloud import storage
 
 def tf_record_dataset(name, sequence_length):
 
-    data = tf.data.TFRecordDataset(filenames=tf.convert_to_tensor([f'gs://{name}']), buffer_size=1, num_parallel_reads=1)
+    #data = tf.data.TFRecordDataset(filenames=tf.convert_to_tensor([f'gs://{name}']), buffer_size=1, num_parallel_reads=1)
     #data = tf.data.TFRecordDataset(filenames=tf.convert_to_tensor(name), buffer_size=1, num_parallel_reads=1)
+    data = tf.data.Dataset.from_tensor_slices(np.random.randint(0, 255, (random.randint(900, 3600), 17, 32, 3)))
 
-    data = data.map(frame_decoder)
+    #data = data.map(frame_decoder)
     data = data.window(size=sequence_length + 1, stride=1, shift=sequence_length, drop_remainder=True)
     data = data.flat_map(lambda x: x.batch(sequence_length + 1, drop_remainder=True))
     data = data.repeat()
@@ -22,8 +24,10 @@ def tf_record_dataset(name, sequence_length):
 def generic_data(params, eval=False):
     sequence_length = params['n_ctx']
     buffer_size = params.get('buffer_size', 16)
-    frame_height = params.get('frame_height', 176)
-    frame_width = params.get('frame_width', 320)
+    #frame_height = params.get('frame_height', 176)
+    #frame_width = params.get('frame_width', 320)
+    frame_height = params.get('frame_height', 17)
+    frame_width = params.get('frame_width', 32)
     color_channels = params.get('color_channels', 3)
     batch_size = params['eval_batch_size' if eval else 'train_batch_size']
 
