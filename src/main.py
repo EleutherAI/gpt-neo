869de682a2ce4bd143344dcdf97f69a371a66958
XMaster96 <XMaster96@gmx.de>
2020-12-11 23:41:29 +0100

    change code structure.

---
+++ src/main.py
@@ -0,0 +1,139 @@
+"""GPT-like model in Mesh-Tensorflow"""
+
+from functools import partial
+import mesh_tensorflow as mtf
+import tensorflow.compat.v1 as tf
+from tensorflow.python.tpu import tpu_config, tpu_estimator
+from tensorflow_estimator.python.estimator import estimator as estimator_lib
+from src.utils import save_config, expand_attention_types_params, yes_or_no, remove_gs_or_filepath, setup_logging, \
+    check_dataset
+from src.inputs import generic_data
+from src.train import model_fn
+from src.utils import fetch_model_params
+import argparse
+
+
+def parse_args():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tpu", type=str, help="Name of TPU to train on, if any.")
+    parser.add_argument("--gpu_ids", nargs="+", type=str, default=["device:GPU:0"],
+                        help=" If training on GPU, can specify your GPU names in a list - i.e 'device:GPU:0 device:GPU:1'")
+    parser.add_argument("--model", type=str, default=None, help="JSON file that contains model parameters.")
+    parser.add_argument("--steps_per_checkpoint", type=int, default=5000, help="Save a model checkpoint every X steps.")
+    parser.add_argument("--auto_layout", action="store_true", help="If set, generates and prints the most memory "
+                                                                   "efficient layout according to MTF auto layout.")
+    parser.add_argument("--auto_layout_and_mesh_shape", action="store_true",
+                        help="If set, generates and prints the most memory efficient layout and mesh shape according to"
+                             " MTF auto layout.")
+    parser.add_argument("--new", action="store_true", help="If set, deletes previous checkpoint, if it exists, and "
+                                                           "starts a new training run")
+    parser.add_argument("--predict", action="store_true", help="If set, uses the model to predict rather than train.")
+    parser.add_argument("--prompt", type=str, help="path to .txt file containing a prompt for prediction. If empty, "
+                                                   "defaults to unicorns.",
+                        default="")
+    parser.add_argument("--check_dataset", action="store_true",
+                        help="If set, outputs sample from the dataset and quits.")
+    args = parser.parse_args()
+    assert args.model is not None, "Model must be set"
+    return args
+
+
+def main(args):
+    # Setup logging
+    logger = setup_logging(args)
+
+    # Read params of model
+    params = fetch_model_params(args.model)
+
+    # Fetch appropriate input functions
+    input_fn = generic_data
+
+    # Sample from Dataset if check dataset flag is on
+    if args.check_dataset:
+        check_dataset(input_fn, params)
+
+    # Confirm deletion of checkpoint files if --new flag is set
+    if args.new:
+        if yes_or_no(f"Are you sure you want to remove '{params['model_path']}' to start afresh?"):
+            remove_gs_or_filepath(params["model_path"])
+        else:
+            exit()
+
+    # Save config to logdir for experiment management
+    # save_config(params, params["model_path"])
+
+    # Add to params: auto_layout, auto_layout_and_mesh_shape, use_tpu, num_cores
+    mesh_shape = mtf.convert_to_shape(params["mesh_shape"])
+    params["num_cores"] = mesh_shape.size
+    params["auto_layout"] = args.auto_layout
+    params["auto_layout_and_mesh_shape"] = args.auto_layout_and_mesh_shape
+    params["use_tpu"] = True if not args.tpu is None else False
+    params["gpu_ids"] = args.gpu_ids
+    params["steps_per_checkpoint"] = args.steps_per_checkpoint
+    # Expand attention types param
+    params["attention_types"] = expand_attention_types_params(params["attention_types"])
+    assert len(params["attention_types"]) == params["n_layer"]  # Assert that the length of expanded list = num layers
+    params["predict_batch_size"] = params.get("predict_batch_size", 1)  # Default to 1
+    params["predict"] = args.predict
+    params['model'] = params.get("model", "GPT") # Default model selection to GPT since it's the only option for now
+
+    # Sample quality of MoE models suffers when using the faster sampling method, so default to slow_sampling if
+    # moe layers are present
+    params["slow_sampling"] = True if params["moe_layers"] is not None else False
+
+    logger.info(f"params = {params}")
+
+
+    # Set up TPUs and Estimator
+    if args.tpu == "colab":
+        tpu_cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver() if params["use_tpu"] else None
+    else:
+        tpu_cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(args.tpu) if params["use_tpu"] else None
+
+    tf.tpu.experimental.initialize_tpu_system(tpu_cluster_resolver)
+
+    config = tpu_config.RunConfig(
+        cluster=tpu_cluster_resolver,
+        model_dir=params["model_path"],
+        save_checkpoints_steps=None,  # Disable the default saver
+        save_checkpoints_secs=None,  # Disable the default saver
+        log_step_count_steps=params["iterations"],
+        save_summary_steps=params["iterations"],
+        tpu_config=tpu_config.TPUConfig(
+            num_shards=mesh_shape.size,
+            iterations_per_loop=params["iterations"],
+            num_cores_per_replica=1,
+            per_host_input_for_training=tpu_config.InputPipelineConfig.BROADCAST))
+
+    estimator = tpu_estimator.TPUEstimator(
+        use_tpu=params["use_tpu"],
+        model_fn=model_fn,
+        config=config,
+        train_batch_size=params["train_batch_size"],
+        predict_batch_size=params["predict_batch_size"],
+        params=params)
+
+    def _make_task_estimator(task):
+        task_params = params.copy()
+        return tpu_estimator.TPUEstimator(
+            use_tpu=params["use_tpu"],
+            model_fn=model_fn,
+            config=config,
+            train_batch_size=params["train_batch_size"],
+            predict_batch_size=params["predict_batch_size"],
+            params=task_params)
+
+
+    current_step = int(estimator_lib._load_global_step_from_checkpoint_dir(params["model_path"]))
+    logger.info(f"Current step {current_step}")
+
+    while current_step < params["train_steps"]:
+        # Else, don't stop and restart
+        estimator.train(input_fn=partial(input_fn, eval=False), max_steps=params["train_steps"])
+
+
+if __name__ == "__main__":
+    tf.disable_v2_behavior()
+    args = parse_args()
+    main(args)
---
+++ src/train.py
@@ -0,0 +1,185 @@
+import mesh_tensorflow as mtf
+import tensorflow.compat.v1 as tf
+from tensorflow.python.tpu import tpu_estimator
+import mesh_tensorflow.transformer as mtf_transformer
+from src.optimizers import get_optimizer
+from src.utils import (create_host_call, get_graph_info, remove_batch_from_layout, simd_mesh_setup, add_mode_to_params, get_batch_size, auto_layout, auto_layout_and_mesh_shape)
+from src import model
+
+def model_fn(features, labels, mode, params):
+    # Get global step
+    global_step = tf.train.get_global_step()
+
+    # Construct mtf graph + mesh from params
+    graph = mtf.Graph()
+    mesh_shape = mtf.convert_to_shape(params["mesh_shape"])
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        params["layout"] = remove_batch_from_layout(params["layout"])
+    layout_rules = mtf.convert_to_layout_rules(params["layout"])
+    
+    # Mesh setup
+    if params["use_tpu"]:
+        var_placer, mesh_impl = simd_mesh_setup(params, mesh_shape, layout_rules)
+    else:
+        var_placer = None
+        gpu_ids = params["gpu_ids"]
+        mesh_shape = [("all_processors", len(gpu_ids))]
+        layout_rules = [("batch", "all_processors")]
+        mesh_impl = mtf.placement_mesh_impl.PlacementMeshImpl(
+            mesh_shape, layout_rules, gpu_ids)
+
+    # Trainable variable precision
+    # Store to checkpoints in master type, train in slice type, compute in activation type
+    variable_dtype = mtf.VariableDType(master_dtype=tf.float32, slice_dtype=tf.float32, activation_dtype=tf.float32)
+
+    # Build mtf mesh object
+    mesh = mtf.Mesh(graph, "my_mesh", var_placer)
+
+    # Build mtf_features & seq length dict for getting number of microbatches
+    # We need to pack inputs into a dict to pass into serialize_training_step
+    features_dict = {"inputs": features, "labels": labels}
+    sequence_length_dict = {"inputs": params["n_ctx"], "labels": params["n_ctx"]}
+
+    params = add_mode_to_params(params, mode)
+    batch_size = get_batch_size(params)
+    
+    model_input = next(iter(features_dict.values()))
+    model_input_shape = model_input.get_shape().as_list()
+    batch_dim = mtf.Dimension("batch", model_input_shape[0])
+    sequence  = mtf.Dimension("sequence", model_input_shape[1])
+    width  = mtf.Dimension("width", model_input_shape[2])
+    height  = mtf.Dimension("height", model_input_shape[3])
+    batch_dims = [batch_dim, sequence, width, height]
+    feature_length = sequence_length_dict["inputs"]
+    length_dim = mtf.Dimension("color_channels",  model_input_shape[4])
+
+    mtf_features = {}
+    for key, x in features_dict.items():
+        if x is not None:
+            feature_shape = mtf.Shape(batch_dims + [length_dim])
+            mtf_features[key] = mtf.import_fully_replicated(
+                mesh, features_dict[key], feature_shape, name=key)
+
+    # Instantiate dict for dimensions, bias, etc that can be calculated here once then passed into model
+    other_features = {}
+    memory_length_dim = mtf.Dimension("memory_length", length_dim.size)
+
+    attn_bias = model.biasmask_attn_weights(mesh, length_dim, memory_length_dim, variable_dtype) if params["causal"] else None
+
+    # Add attn_bias into mtf_features
+    other_features["attn_bias"] = attn_bias
+
+    # Define other Dimensions that we'll need inside the model
+    embd_dim = mtf.Dimension("embd", params["n_embd"])
+    vocab_dim = mtf.Dimension("vocab", params["n_vocab"])
+    # We need this because gathering when both the args have the same dimension in them breaks things
+    # This dim is specifically for the weights
+    # This prevents the "Einsum has lhs dimension without corresponding rhs or output dimension." error
+    embed_sequence_dim = mtf.Dimension("embed_sequence", params["n_ctx"])
+
+    other_features["embd_dim"] = embd_dim
+    other_features["vocab_dim"] = vocab_dim
+    other_features["embed_sequence_dim"] = embed_sequence_dim
+    other_features["memory_length_dim"] = memory_length_dim
+
+    # We're not predicting, so we better be training or evaluating
+    assert (mode == tf.estimator.ModeKeys.TRAIN or mode == tf.estimator.ModeKeys.EVAL)
+
+    # Gets number of microbatches per batch for serialized training
+    # if param tokens_per_mb_per_replica = None, this defaults to 1 and no microbatching is performed
+    num_microbatches = int(mtf_transformer.utils.serialize_num_microbatches(batch_dim=batch_dim,
+                                                                        sequence_length=sequence_length_dict,
+                                                                        mesh_shape=mesh_shape,
+                                                                        layout_rules=layout_rules,
+                                                                        tokens_per_microbatch_per_replica=params["tokens_per_mb_per_replica"]))
+    params["num_microbatches"] = num_microbatches  # Add num microbatches to params
+    if num_microbatches > 1:
+        # For serialize_training_step we need to modify the model to output results in a dict
+        def serialized_fn(mtf_features):
+            if params["model"] == "GPT":
+                with tf.variable_scope('gpt2'):
+                    logits, loss = model.model(mtf_features, other_features, params, mesh, variable_dtype=variable_dtype)
+                return {"logits": logits, "loss": loss}
+            else:
+                raise Exception(f"'{params['model']}' is not a valid model - please select from [GPT]")
+
+        # Serialize the training step - Gradients are accumulated locally and reduced once.
+        var_grads, output_dict = mtf.serialize_training_step(mtf_features, serialized_fn, batch_dim, num_microbatches)
+        loss = output_dict["loss"]
+        logits = output_dict["logits"]
+    else:
+        # If we're not splitting into microbatches, return logits & loss as is
+        if params["model"] == "GPT":
+            with mtf.utils.outside_all_rewrites():
+                with tf.variable_scope('gpt2'):
+                    logits, loss = model.model(mtf_features, other_features, params, mesh, variable_dtype=variable_dtype)
+        else:
+            raise Exception(f"'{params['model']}' is not a valid model - please select from [GPT]")
+
+    # Auto layout generation
+    if params["auto_layout"]:
+        auto_layout(graph, mesh_shape, logits, loss)
+    if params["auto_layout_and_mesh_shape"]:
+        auto_layout_and_mesh_shape(graph, params["num_cores"], logits, loss)
+
+    # In TRAIN mode, get optimizer
+    if params["num_microbatches"] > 1:
+        # If we are splitting the batch into microbatches, var grads are created in the serialize_training_step fn
+        # So we pass them in here
+        _, update_ops, var_grads = get_optimizer(mesh, loss, params, variable_dtype=variable_dtype, inp_var_grads=var_grads)
+    else:
+        # Otherwise, they are created in the get_optimizer fn, so we leave inp_var_grads blank
+        _, update_ops, var_grads = get_optimizer(mesh, loss, params, variable_dtype=variable_dtype)
+    # Log summaries to tensorboard
+    mtf.scalar_summary("loss", loss)
+    # Log gradients if in params
+    if params["log_grads"] not in [None, False]:
+        for g in var_grads:
+            grad_norm = mtf.sqrt(mtf.reduce_sum(mtf.square(g)))
+            mtf.scalar_summary("grads/norm" + g.name[:-2], grad_norm)
+
+    # Gets & prints info about no. trainable vars in the model & dimension names
+    get_graph_info(graph)
+
+    lowering = mtf.Lowering(graph, {mesh: mesh_impl}, autostack=True)
+                            
+    tf_loss = lowering.export_to_tf_tensor(loss)
+    tf_loss = tf.cast(tf_loss, tf.float32)
+
+    # Use our patched version until mtf updates theirs
+    host_call = create_host_call(params['model_path'], labels)
+    mtf.utils.remove_summaries()
+
+    # Creates train_op
+    tf_update_ops = [lowering.lowered_operation(op) for op in update_ops]
+    tf_update_ops.append(tf.assign_add(global_step, 1))  # Need to manually increment global_step
+    tf.logging.info(f"tf_update_ops: {tf_update_ops}")
+    train_op = tf.group(tf_update_ops)
+
+    with mtf.utils.outside_all_rewrites():
+
+        # Copy master variables to slices. Must be called first.
+        restore_hook = mtf.MtfRestoreHook(lowering)
+
+
+        saver = tf.train.Saver(
+            tf.global_variables(),
+            sharded=True,
+            max_to_keep=10,
+            keep_checkpoint_every_n_hours=2,
+            defer_build=False,
+            save_relative_paths=True)
+        tf.add_to_collection(tf.GraphKeys.SAVERS, saver)
+        saver_listener = mtf.MtfCheckpointSaverListener(lowering)
+        saver_hook = tf.train.CheckpointSaverHook(
+            params["model_path"],
+            save_steps=params["steps_per_checkpoint"],
+            saver=saver,
+            listeners=[saver_listener])
+
+        return tpu_estimator.TPUEstimatorSpec(
+            tf.estimator.ModeKeys.TRAIN,
+            loss=tf_loss,
+            host_call=host_call,
+            train_op=train_op,
+            training_hooks=[restore_hook, saver_hook])
