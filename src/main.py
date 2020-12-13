e1c8ae989b151f4a8fd7c6187dd992dd64d53bca
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2020-12-13 10:53:44 +0100

    style(config): use dataclass (instead of dict)

--- src/main.py
+++ src/main.py
@@ -1,20 +1,15 @@
-from functools import partial
 import argparse
+from functools import partial
 
 import mesh_tensorflow as mtf
-
-from tensorflow_estimator.python.estimator import estimator as estimator_lib
-from tensorflow.python.tpu import tpu_config, tpu_estimator
-from utils import expand_attention_types_params
-from utils import remove_gs_or_filepath
-from utils import fetch_model_params
 import tensorflow.compat.v1 as tf
-from utils import setup_logging
-from utils import check_dataset
-from inputs import generic_data
-from utils import save_config
-from utils import yes_or_no
-from train import model_fn
+from tensorflow.python.tpu import tpu_config, tpu_estimator
+from tensorflow_estimator.python.estimator import estimator as estimator_lib
+
+from .dataclass import ModelParameter
+from .inputs import generic_data
+from .train import model_fn
+from .utils import check_dataset, fetch_model_params, remove_gs_or_filepath, setup_logging, yes_or_no
 
 
 def parse_args():
@@ -44,12 +39,11 @@ def parse_args():
 
 
 def main(args: argparse.Namespace):
-
     # Setup logging
     logger = setup_logging(args)
 
     # Read params of model
-    params = fetch_model_params(args.model)
+    params = ModelParameter(fetch_model_params(args.model))
 
     # Fetch appropriate input functions
     input_fn = generic_data
@@ -74,8 +68,6 @@ def main(args: argparse.Namespace):
     params["gpu_ids"] = args.gpu_ids
     params["steps_per_checkpoint"] = args.steps_per_checkpoint
     # Expand attention types param
-    params["attention_types"] = expand_attention_types_params(params["attention_types"])
-    assert len(params["attention_types"]) == params["n_layer"]  # Assert that the length of expanded list = num layers
     params["predict_batch_size"] = params.get("predict_batch_size", 1)  # Default to 1
     params["predict"] = args.predict
     params['model'] = params.get("model", "GPT")  # Default model selection to GPT since it's the only option for now
@@ -96,35 +88,35 @@ def main(args: argparse.Namespace):
     tf.tpu.experimental.initialize_tpu_system(tpu_cluster_resolver)
 
     config = tpu_config.RunConfig(
-        cluster=tpu_cluster_resolver,
-        model_dir=params["model_path"],
-        save_checkpoints_steps=None,  # Disable the default saver
-        save_checkpoints_secs=None,  # Disable the default saver
-        log_step_count_steps=params["iterations"],
-        save_summary_steps=params["iterations"],
-        tpu_config=tpu_config.TPUConfig(
-            num_shards=mesh_shape.size,
-            iterations_per_loop=params["iterations"],
-            num_cores_per_replica=1,
-            per_host_input_for_training=tpu_config.InputPipelineConfig.BROADCAST))
+            cluster=tpu_cluster_resolver,
+            model_dir=params["model_path"],
+            save_checkpoints_steps=None,  # Disable the default saver
+            save_checkpoints_secs=None,  # Disable the default saver
+            log_step_count_steps=params["iterations"],
+            save_summary_steps=params["iterations"],
+            tpu_config=tpu_config.TPUConfig(
+                    num_shards=mesh_shape.size,
+                    iterations_per_loop=params["iterations"],
+                    num_cores_per_replica=1,
+                    per_host_input_for_training=tpu_config.InputPipelineConfig.BROADCAST))
 
     estimator = tpu_estimator.TPUEstimator(
-        use_tpu=params["use_tpu"],
-        model_fn=model_fn,
-        config=config,
-        train_batch_size=params["train_batch_size"],
-        predict_batch_size=params["predict_batch_size"],
-        params=params)
-
-    def _make_task_estimator(task):
-        task_params = params.copy()
-        return tpu_estimator.TPUEstimator(
             use_tpu=params["use_tpu"],
             model_fn=model_fn,
             config=config,
             train_batch_size=params["train_batch_size"],
             predict_batch_size=params["predict_batch_size"],
-            params=task_params)
+            params=params)
+
+    def _make_task_estimator(task):
+        task_params = params.copy()
+        return tpu_estimator.TPUEstimator(
+                use_tpu=params["use_tpu"],
+                model_fn=model_fn,
+                config=config,
+                train_batch_size=params["train_batch_size"],
+                predict_batch_size=params["predict_batch_size"],
+                params=task_params)
 
     current_step = int(estimator_lib._load_global_step_from_checkpoint_dir(params["model_path"]))
     logger.info(f"Current step {current_step}")
