fca6682d158fb8ffa2b9eaa6025c954de2957f9a
Luke <luke@clashproject.com>
2020-12-13 13:21:45 +0100

    fix(model): dont reference shape of shape

--- src/model.py
+++ src/model.py
@@ -89,7 +89,7 @@ def model(mtf_features: dict, other_features: dict, params: collections.defaultd
                 return block_input
 
         output = mtf.recompute_grad(_block_fn, [output])
-    output = generic_feed_forward(output, [dim_heads, key_dim], original_shape.shape[-1:], tf.float32, dropout_rate)
+    output = generic_feed_forward(output, [dim_heads, key_dim], original_shape[-1:], tf.float32, dropout_rate)
     output = mtf.reshape(output, original_shape)
 
     with tf.variable_scope("reduce_mean_final"):
