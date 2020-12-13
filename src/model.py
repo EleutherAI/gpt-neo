733aa120aabf74b4f534c1f5a4953e688964760e
Luke <luke@clashproject.com>
2020-12-13 09:37:15 +0100

    style(model): refactor common code away

--- src/model.py
+++ src/model.py
@@ -1,32 +1,33 @@
 import collections
+import random
 import typing
 
 import mesh_tensorflow as mtf
-import mesh_tensorflow.transformer as mtf_transformer
 import tensorflow.compat.v1 as tf
 
-sentinel = object()
 
-
-def rezero(x: tf.Tensor, scope: str, dtype: mtf.VariableDType):
-    with tf.variable_scope(scope):
+def rezero(x: tf.Tensor, dtype: mtf.VariableDType):
+    with tf.variable_scope(f'rezero_{random.getrandbits(64):x}'):
         g = mtf.get_variable(x.mesh, "g", [], initializer=tf.constant_initializer(0), dtype=dtype)
-        return x * g
+        x = x * g
+    return x
 
 
-def feed_forward(x: mtf.Tensor,
-                 nf: float,
-                 variable_dtype: typing.Union[mtf.VariableDType, tf.DType] = tf.float32,
-                 dropout_rate: float = 0):
-    x = mtf.layers.dense(x, new_dims=[nf], reduced_dims=[x.shape[-1]], use_bias=True,
-                         kernel_initializer=tf.orthogonal_initializer(),
-                         variable_dtype=variable_dtype, name="feed_forward0")
-    if dropout_rate > 0:
-        x = mtf.dropout(x, 1 - dropout_rate)
-    x = mtf.gelu(x)
-    x = mtf.layers.dense(x, new_dims=[nf], reduced_dims=[nf], use_bias=True,
-                         kernel_initializer=tf.orthogonal_initializer(),
-                         variable_dtype=variable_dtype, name="feed_forward1")
+def generic_feed_forward(x: mtf.Tensor,
+                         reduced_dims: typing.List[mtf.Dimension],
+                         new_dimensions: typing.List[mtf.Dimension],
+                         variable_dtype: typing.Union[mtf.VariableDType, tf.DType] = tf.float32,
+                         dropout_rate: float = 0):
+    with tf.variable_scope(f'feed_forward_{random.getrandbits(64):x}'):
+        x = mtf.layers.dense(x, new_dims=new_dimensions, reduced_dims=reduced_dims, use_bias=True,
+                             kernel_initializer=tf.orthogonal_initializer(),
+                             variable_dtype=variable_dtype, name="dense0")
+        if dropout_rate > 0:
+            x = mtf.dropout(x, 1 - dropout_rate)
+        x = mtf.gelu(x)
+        x = mtf.layers.dense(x, new_dims=new_dimensions, reduced_dims=new_dimensions, use_bias=True,
+                             kernel_initializer=tf.orthogonal_initializer(),
+                             variable_dtype=variable_dtype, name="dense1")
     return x
 
 
@@ -36,67 +37,64 @@ def feed_forward(x: mtf.Tensor,
 def model(mtf_features: dict, other_features: dict, params: collections.defaultdict, mesh: mtf.Mesh,
           variable_dtype: mtf.VariableDType):
     """A GPT style model implemented in mesh tensorflow."""
-
+    embd_dim = other_features["embd_dim"]
     x = mtf_features["inputs"]
-
+    original_shape = x.shape
     sequence_dim = x.shape[1]
     width = x.shape[2]
     height = x.shape[3]
-    dropout_rate = params['dropout_rate']
+    dropout_rate = params.get('dropout_rate', 0)
 
-    embd_dim = other_features["embd_dim"]
+    dim_heads = mtf.Dimension("heads", params["n_head"])
+    key_dim = mtf.Dimension("features_per_head", embd_dim.size // params["n_head"])
 
-    h = feed_forward(x, embd_dim, tf.float32, dropout_rate)
+    h = generic_feed_forward(x, x.shape[-1:], [dim_heads, key_dim], tf.float32, dropout_rate)
+
+    def _feed_forward(x):
+        return generic_feed_forward(x, [dim_heads, key_dim], [dim_heads, key_dim], tf.float32, dropout_rate)
 
     for layer in range(params["n_layer"]):
-        def block_fn(x):
+        def _block_fn(x):
             with tf.variable_scope(f"h{layer}"):
                 summed_a = None
 
                 for idx, dim in enumerate([sequence_dim, width, height]):
                     tmp_dim = mtf.Dimension(f'anonymous_{dim.name}', dim.size)
-                    dim_heads = mtf.Dimension("heads", params["n_head"])
-                    key_dim = mtf.Dimension("features_per_head", embd_dim.size // params["n_head"])
 
                     with tf.variable_scope(f"attn{dim.name}"):
-                        mtfparams = mtf.transformer.attention.attention_params_simple(x.mesh,
-                                                                                      io_dim=embd_dim,
-                                                                                      kv_dim=key_dim,
-                                                                                      heads_dim=dim_heads,
-                                                                                      variable_dtype=variable_dtype
-                                                                                      )
-                        q = mtfparams.compute_q(x)
-                        k = mtfparams.compute_k(x)
-                        v = mtfparams.compute_v(x)
+                        q = _feed_forward(x)
+                        k = _feed_forward(x)
+                        v = _feed_forward(x)
                         k = mtf.rename_dimension(k, dim.name, tmp_dim.name)
                         v = mtf.rename_dimension(v, dim.name, tmp_dim.name)
 
                         with tf.variable_scope("attention"):
-                            logits = mtf.einsum([q, k], q.shape - key_dim + tmp_dim)
+                            logits = mtf.einsum([q, k], q.shape - key_dim + tmp_dim) / tmp_dim.size ** 0.5
                             if idx == 0:
                                 i = mtf.range(mesh, tmp_dim, tf.int32) + dim.size - tmp_dim.size
                                 j = mtf.range(mesh, dim, tf.int32)
-                                i, j = map(lambda t: mtf.broadcast(t, [tmp_dim, dim]), (i, j))
+                                i = mtf.broadcast(i, [tmp_dim, dim])
+                                j = mtf.broadcast(j, [tmp_dim, dim])
                                 bias = mtf.cast(mtf.less(i, j), variable_dtype.activation_dtype) * -1e12
                                 logits += mtf.broadcast(bias, logits.shape)
                             weights = mtf.softmax(logits, dim)
                             a = mtf.einsum([weights, v], q.shape)
 
-                        with tf.variable_scope("compute_output"):
-                            a = mtfparams.compute_output(a, x.shape)
+                        a = _feed_forward(a)
 
                     summed_a = a if summed_a is None else (summed_a + a)
 
-                x = x + rezero(summed_a, "norm_rezero_1", variable_dtype)
+                x = x + rezero(summed_a, variable_dtype)
 
                 with tf.variable_scope(f"h{layer}"):
-                    m = feed_forward(x, embd_dim, variable_dtype=tf.float32)
+                    m = _feed_forward(x)
 
-                x = x + rezero(m, "norm_rezero_2", variable_dtype)
+                x = x + rezero(m, variable_dtype)
                 return x
 
-        h = mtf.recompute_grad(block_fn, [h])
+        h = mtf.recompute_grad(_block_fn, [h])
 
+    h = mtf.reshape(h, original_shape)
     output = mtf.cast(h, tf.float32)
 
     with tf.variable_scope("reduce_mean_final"):
