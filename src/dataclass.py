972b3166597ffaea2199d589fd1ee4d8918608a3
XMaster96 <XMaster96@gmx.de>
2020-12-22 07:52:23 +0100

    language token fix test v19

--- src/dataclass.py
+++ src/dataclass.py
@@ -191,7 +191,7 @@ class ModelParameter(dict):
 
         return block_output
 
-    def build(self, model_input):
+    def build(self, model_input, token_x_input, token_y_input):
         """A GPT style model implemented in mesh tensorflow."""
         x = model_input / 255.
         context_dimension = x.shape[1]
