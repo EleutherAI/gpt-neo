8e9d0d67142cbb422486681cfdadaf3e413e21dc
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-01-02 00:35:14 +0000

    perf(model): revert to performant code

--- src/dataclass.py
+++ src/dataclass.py
@@ -1,14 +1,21 @@
-import base64
 import random
 import typing
 
 import mesh_tensorflow as mtf
+import numpy as np
 import tensorflow.compat.v1 as tf
 
 
-def random_name():
-    return base64.b64encode(random.getrandbits(256).to_bytes(length=32, byteorder='little')).decode().replace("+", "").replace("/", "").replace("=", "")
+def random_name(prefix: str):
+    return f"{prefix}_{random.getrandbits(64):x}"
 
+
+def anonymize(inp: mtf.Tensor, dim_name: str):
+    return mtf.rename_dimension(inp, dim_name, '_' + dim_name)
+
+
+def unanonymize(inp: mtf.Tensor, dim_name: str):
+    return mtf.rename_dimension(inp, '_' + dim_name, dim_name)
 
 
 class ModelParameter(dict):
@@ -77,7 +84,8 @@ class ModelParameter(dict):
         self.activation_function = "gelu"
         self.gradient_clipping = 1.0
         self.dropout_rate = 0.
-        self.token_embedding = False
+        self.feed_forward_per_attention = 2
+        self.intermediate_feed_forward_multiplier = 1
 
         self.mesh = None
 
@@ -92,18 +100,10 @@ class ModelParameter(dict):
         self.frame_width_patch = self.frame_width // self.patch_size
         self.channel_color_size = self.color_channels * self.time_patch * self.patch_size ** 2
         self.the_batch_size = self.eval_batch_size if self.eval else self.train_batch_size
-
-    @property
-    def dim_heads(self):
-        return mtf.Dimension("heads", self.n_head)
-
-    @property
-    def key_dim(self):
-        return mtf.Dimension("features_per_head", self.n_embd // self.n_head)
-
-    @property
-    def feature_dims(self):
-        return [self.dim_heads, self.key_dim]
+        self.use_language = self.language_token_per_frame > 0
+        self.dim_heads = mtf.Dimension("heads", self.n_head)
+        self.key_dim = mtf.Dimension("features_per_head", self.n_embd // self.n_head)
+        self.feature_dims = [self.dim_heads, self.key_dim]
 
     def __getitem__(self, key):
         print(f"Getting {key} via deprecated interface")
@@ -139,113 +139,110 @@ class ModelParameter(dict):
         return self.__dict__
 
     def _get_variable(self, shape, initializer):
-        return mtf.get_variable(self.mesh, random_name(),
-                                shape,
-                                dtype=tf.float32,
-                                initializer=initializer)
+        return mtf.get_variable(self.mesh, random_name("variable"), shape, dtype=tf.float32, initializer=initializer)
 
-    def _rezero(self, block_input: tf.Tensor):
-        return block_input * self._get_variable([], tf.constant_initializer(0))
+    def _get_scalar(self, value):
+        return self._get_variable([], tf.constant_initializer(value))
+
+    def _rezero(self, block_input: mtf.Tensor, init: float = 0.):
+        with tf.variable_scope(random_name("rezero")):
+            return block_input * self._get_scalar(init)
+
+    def _linear(self, block_input: mtf.Tensor, old: typing.List[mtf.Dimension], new: typing.List[mtf.Dimension]):
+        with tf.variable_scope(random_name('linear')):
+            return mtf.einsum([block_input, self._get_variable(old + new, tf.orthogonal_initializer())],
+                              block_input.shape - old + new)
 
     def _generic_feed_forward(self,
                               block_input: mtf.Tensor,
                               reduced: typing.List[mtf.Dimension],
                               new: typing.List[mtf.Dimension],
                               intermediate_factor: float = 1.):
-        intermediate = [mtf.Dimension('_' + dim.name, dim.size) for dim in new]
-        with tf.variable_scope(random_name()):
-            weight0 = self._get_variable(reduced + intermediate, tf.orthogonal_initializer())
-            block_input = mtf.einsum([block_input, weight0],
-                                     block_input.shape - reduced + intermediate)
-            if self.dropout_rate > 0:
-                block_input = mtf.dropout(block_input, 1 - self.dropout_rate)
+        intermediate = [mtf.Dimension('_intermediate',
+                                      int(np.prod([dim.size for dim in new])
+                                          * intermediate_factor
+                                          * self.intermediate_feed_forward_multiplier))]
+        with tf.variable_scope(random_name("feed_forward")):
+            block_input = self._linear(block_input, reduced, intermediate)
+            block_input = mtf.dropout(block_input, rate=self.dropout_rate)
             block_input = block_input * mtf.tanh(block_input)  # LiSHT: https://arxiv.org/abs/1901.05894
-            weight1 = self._get_variable(intermediate + new, tf.orthogonal_initializer())
-            block_input = mtf.einsum([block_input, weight1],
-                                     block_input.shape - intermediate + new)
-        return block_input
+            return self._linear(block_input, intermediate, new)
 
-
-    def _feed_forward(self, x):
-        return self._generic_feed_forward(x, self.feature_dims, self.feature_dims)
+    def _feed_forward(self, x: mtf.Tensor, intermediate_factor: float = 1.):
+        return self._generic_feed_forward(x, self.feature_dims, self.feature_dims, intermediate_factor)
 
     def _block_fn(self, block_input):
         self._layer_idx += 1
+        attention_dims = (block_input.shape - self.feature_dims)[1:]  # Ex: Shape[Sequence, Width, Height]
 
-        if self._layer_idx % 2:
-            with tf.variable_scope(random_name()):
-                output = self._rezero(self._feed_forward(block_input))
-            return output
+        if self._layer_idx % (self.feed_forward_per_attention + 1) < self.feed_forward_per_attention:
+            with tf.variable_scope(f"feed_forward_block_{self._layer_idx}"):
+                block_input = mtf.add_n([self._feed_forward(block_input, 0.5 ** (len(attention_dims).bit_length() - 1))
+                                         * (mtf.range(self.mesh, dim, tf.float32) + 1)
+                                         / dim.size
+                                         for dim in attention_dims] + [block_input])
+                return self._rezero(self._feed_forward(block_input))
 
-        with tf.variable_scope(random_name()):
-            attention_dims = block_input.shape[1:-2]  # Ex: Shape[Sequence, Width, Height]
-            idx = (self._layer_idx // 2) % len(attention_dims)
-            dim = attention_dims[idx]
+        idx = (self._layer_idx // (self.feed_forward_per_attention + 1)) % len(attention_dims)
+        dim = attention_dims[idx]
+        tmp_dim = mtf.Dimension(f'_{dim.name}', dim.size)
 
-            tmp_dim = mtf.Dimension(f'anonymous_{dim.name}', dim.size)
-
+        with tf.variable_scope(f"attention_block_{self._layer_idx}"):
             q = self._feed_forward(block_input)
-            k = self._feed_forward(block_input)
-            v = self._feed_forward(block_input)
-            k = mtf.rename_dimension(k, dim.name, tmp_dim.name)
-            v = mtf.rename_dimension(v, dim.name, tmp_dim.name)
+            k = anonymize(self._feed_forward(block_input), dim.name)
+            v = anonymize(self._feed_forward(block_input), dim.name)
 
-            logits = mtf.einsum([q, k], q.shape - self.key_dim + tmp_dim) / tmp_dim.size ** 0.5
+            logits = mtf.einsum([q, k], reduced_dims=[self.key_dim]) / dim.size ** 0.5
             if idx in self.masked_attention_dimensions:
-                i = mtf.range(self.mesh, tmp_dim, tf.int32) + dim.size - tmp_dim.size
+                i = mtf.range(self.mesh, tmp_dim, tf.int32)
                 j = mtf.range(self.mesh, dim, tf.int32)
                 i = mtf.broadcast(i, [tmp_dim, dim])
                 j = mtf.broadcast(j, [tmp_dim, dim])
-                bias = mtf.cast(mtf.less(i, j), tf.float32) * -1e12
-                logits += mtf.broadcast(bias, logits.shape)
+                logits += mtf.cast(mtf.less(i, j), tf.float32) * -1e12
             weights = mtf.softmax(logits, dim)
             output = mtf.einsum([weights, v], q.shape)
-            output = self._rezero(output)
+            return self._rezero(output)
 
-        return output
+    def build(self, model_input, tkn_src, tkn_tgt):
+        vocab_dim = mtf.Dimension("vocab_size", self.vocab_size)
 
-    def build(self, model_input):
-        # TODO: Add support for missing model_input, token_x/y_input
-        # TODO: Rename token_x_input
-        # TODO: General cleanup
-        x = model_input / 255.
+        x = model_input / self._get_scalar(127.5) + self._get_scalar(-1)
         context_dimension = x.shape[1]
+        input_features = x.shape[-1:]
+        spatial_ctx = x.shape[2]
 
         tgt = mtf.slice(x, 1, context_dimension.size - 1, context_dimension.name)
         src = mtf.slice(x, 0, context_dimension.size - 1, context_dimension.name)
 
-        if self.token_embedding:
-            input_features = [mtf.Dimension("vocab_size", self.vocab_size)]
-            embedding = mtf.get_variable(x.mesh, random_name(), mtf.Shape(input_features + self.feature_dims),
-                                         dtype=tf.float32, initializer=tf.random_normal_initializer())
-            src = mtf.einsum([mtf.one_hot(token_x_input, input_features[0], dtype=tf.float32), embedding],
-                             reduced_dims=input_features,
-                             output_shape=token_x_input.shape + self.feature_dims)
+        src = self._linear(src, input_features, self.feature_dims)
 
-        else:
-            input_features = [src.shape[-1]]
-            src = self._generic_feed_forward(src, input_features, self.feature_dims)
+        if self.use_language:
+            tkn_src = self._linear(mtf.one_hot(tkn_src, vocab_dim, dtype=tf.float32), [vocab_dim], self.feature_dims)
+            src = unanonymize(mtf.concat([anonymize(src, spatial_ctx.name), anonymize(tkn_src, spatial_ctx.name)],
+                                         '_' + spatial_ctx.name),
+                              spatial_ctx.name)
 
-        src_embedding = mtf.add_n([self._get_variable([dim] + self.feature_dims, tf.random_normal_initializer())
-                                   for dim in src.shape[1:-2]]  # Ex: Shape[Sequence, Width, Height]
-                                  )
-        src += src_embedding
-
-        xs = (self._generic_feed_forward(src, self.feature_dims, self.feature_dims), None,
-              self._generic_feed_forward(src, self.feature_dims, self.feature_dims), None)
+        xs = (src, None, src, None)
 
         for layer in range(self.n_layer):
             xs = mtf.layers.reversible_half_residual_and_swap(*xs, self._block_fn)
 
-        src = self._generic_feed_forward(xs[0] + xs[2], self.feature_dims, input_features)
+        out = self._rezero(xs[0], 1) + self._rezero(xs[2], 1)
+        loss = 0
 
-        with tf.variable_scope(random_name()):
-            if self.token_embedding:
-                loss = mtf.reduce_mean(mtf.layers.softmax_cross_entropy_with_logits(logits=src, targets=tgt,
-                                                                                    vocab_dim=input_features,
-                                                                                    z_loss=1e-4))
-            else:
-                loss = mtf.reduce_mean(mtf.abs(src - tgt))
+        if self.use_language:
+            out = anonymize(out, spatial_ctx)
+            tkn = unanonymize(mtf.slice(out, spatial_ctx.size, self.language_token_per_frame, '_' + spatial_ctx.name),
+                              spatial_ctx.name)
+            out = unanonymize(mtf.slice(out, 0, spatial_ctx.size, '_' + spatial_ctx.name), spatial_ctx.name)
+            tkn = self._generic_feed_forward(tkn, self.feature_dims, [vocab_dim])
+            loss = mtf.reduce_mean(mtf.layers.softmax_cross_entropy_with_logits(logits=tkn,
+                                                                                targets=tkn_tgt,
+                                                                                vocab_dim=vocab_dim,
+                                                                                z_loss=1e-4))
+
+        src = self._generic_feed_forward(out, self.feature_dims, input_features)
+        loss += mtf.reduce_mean(mtf.abs(src - tgt))
 
         self._layer_idx = 0
 
