cca19a09f61810300ebb684ac6f4a3b0f86f9b09
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-02-09 00:12:21 +0100

    fix(input): add missing map to text input

--- src/inputs.py
+++ src/inputs.py
@@ -8,7 +8,7 @@ from itertools import cycle
 
 import tensorflow.compat.v1 as tf
 
-from .dataclass import ModelParameter
+from .dataclass import ModelParameter, align_tensor_op
 
 
 def get_video_decoder(language_token_num_per_frame=0, frame_height=None, frame_width=None, color_channels=None):
@@ -329,7 +329,7 @@ def dataset(params: ModelParameter, step: int = 0, train: bool = True):
         datasets = datasets[0]
 
     datasets = datasets.map(memory_op)
-    datasets = datasets.map(params.align_tensor_op)
+    datasets = datasets.map(align_tensor_op)
     datasets = datasets.skip(step)
 
     return datasets
@@ -444,6 +444,7 @@ def gpt_neo_input(params, step=None, eval=False):
     dataset = dataset.shuffle(512, seed=seed)
 
     dataset = dataset.map(_memory_func)
+    dataset = dataset.map(align_tensor_op)
     dataset = dataset.prefetch(params.buffer_size * 2)
 
     options = tf.data.Options()
